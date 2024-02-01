c45dd54b470528474fb5f7bab3f1da28f9b4f752
edenbergel <bergeleden@gmail.com>
2024-02-01 16:53:39 +0100

    Stylize the card

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,14 +11,19 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack(alignment: .leading) {
-            Text(scrum.title).font(.headline)
+            Text(scrum.title)
+                .font(.headline)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
             }
+            .font(.caption)
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
     }
 }
 
