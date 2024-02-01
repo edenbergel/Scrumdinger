f41e9d17331659645eaba4fc088c517d179f3221
edenbergel <bergeleden@gmail.com>
2024-02-01 16:43:16 +0100

    add meeting length data

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -15,6 +15,8 @@ struct CardView: View {
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
             }
         }
     }
