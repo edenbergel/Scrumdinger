90663087b4a1a0e52488694215677e6e8a5393c3
edenbergel <bergeleden@gmail.com>
2024-02-01 19:21:41 +0100

    Create the detail view

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,21 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by eden bergel on 01/02/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
