784f58a20a7d48eee125b6f0e863b928a63888e2
edenbergel <bergeleden@gmail.com>
2024-02-01 22:04:50 +0100

    Add visual components to the detail view

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -10,7 +10,32 @@ import SwiftUI
 struct DetailView: View {
     let scrum: DailyScrum
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Section(header: Text("Meeting Info")) {
+               Label("Start Meeting", systemImage: "timer")
+                    .accessibilityLabel("Meeting Entry")
+                    .foregroundColor(.accentColor)
+                    .font(.headline)
+                
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(6)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(6)
+                }
+                .accessibilityElement(children: .combine)
+            }
+        }
     }
 }
 
