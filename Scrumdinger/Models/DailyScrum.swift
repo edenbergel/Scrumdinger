e9820c5539e3cdf1f0aa58bd2c1013998c06f992
edenbergel <bergeleden@gmail.com>
2024-02-01 19:15:49 +0100

    Set up navigation and change bg color for scrums item

--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -28,16 +28,16 @@ extension DailyScrum {
     static let sampleData: [DailyScrum] =
     [
         DailyScrum(title: "Stand Up The Good",
-            attendees: ["Toto", "TomTom", "Nini", "Denden"],
-            lengthInMinutes: 15,
-            theme: .orange),
+                   attendees: ["Toto", "TomTom", "Nini", "Denden"],
+                   lengthInMinutes: 15,
+                   theme: .yellow),
         DailyScrum(title: "Stand Up The Bad",
-            attendees: ["Leti", "Mel", "Anto", "Kanye"],
-            lengthInMinutes: 20,
-            theme: .lavender),
+                   attendees: ["Leti", "Mel", "Anto", "Kanye"],
+                   lengthInMinutes: 20,
+                   theme: .orange),
         DailyScrum(title: "Stand Up The Ugly",
-            attendees: ["Al", "Vinc", "Bass", "Riri"],
-            lengthInMinutes: 10,
-            theme: .bubblegum)
+                   attendees: ["Al", "Vinc", "Bass", "Riri"],
+                   lengthInMinutes: 10,
+                   theme: .poppy)
     ]
 }
--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -12,9 +12,22 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                ToolbarItem() {
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                        Image(systemName: "plus")
+                    }
+                    .accessibilityLabel("New Scrum")
+                }
+            }
         }
     }
 }
