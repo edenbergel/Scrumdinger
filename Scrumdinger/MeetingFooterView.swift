b922bd07985d7b070c96c5476103dec2f69489ef
edenbergel <bergeleden@gmail.com>
2024-02-03 20:17:54 +0100

    Add life cycle events

---
+++ Scrumdinger/MeetingFooterView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingFooterView.swift
+//  Scrumdinger
+//
+//  Created by eden bergel on 03/02/2024.
+//
+
+import SwiftUI
+
+struct MeetingFooterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingFooterView()
+}
